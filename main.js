ca608c6061f7eb1c9999ea89abb9460c2a72dcb5
Heroplan <1622847724@qq.com>
2025-09-03 23:46:04 +0800

    更新蔬菜英雄

--- main.js
+++ main.js
@@ -44,7 +44,7 @@ function parseAndStoreDoTInfo(hero) {
         const lowerEffectText = effectText.toLowerCase();
 
         // 【排除规则】如果描述中包含 'immune' 'resisted'，则直接跳过此行
-        if (lowerEffectText.includes('immune') | lowerEffectText.includes('resisted') | lowerEffectText.includes('奔涌') | lowerEffectText.includes('Surge') ) {
+        if (lowerEffectText.includes('immune') | lowerEffectText.includes('resisted') | lowerEffectText.includes('fiend') | lowerEffectText.includes('恶魔') | lowerEffectText.includes('惡魔') | lowerEffectText.includes('奔涌') | lowerEffectText.includes('surge') ) {
             return;
         }
 

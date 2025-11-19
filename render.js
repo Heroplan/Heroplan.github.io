5229896e4dbfdaf2ec733f9c4756e286dfb9bed0
Heroplan <1622847724@qq.com>
2025-11-19 10:02:59 +0800

    Update render.js

--- render.js
+++ render.js
@@ -625,7 +625,7 @@ function applyKeywordHighlighting(text, lang, filterType) {
     let highlightedPrefix = '';
 
     // --- 如果 filterType 是 'effects' 或 'passives'，则进行特殊预处理 ---
-    if ((filterType === 'effects' || filterType === 'passives') ) {
+    if ((filterType === 'effects' || filterType === 'passives')) {
 
         const lengthLimit = (lang && lang.startsWith('en')) ? 75 : 30;
         const hasColon = (text.endsWith(':') || text.endsWith('：')) && text.length < lengthLimit;
@@ -731,7 +731,7 @@ function applyKeywordHighlighting(text, lang, filterType) {
             }
         }
         // --- 单独规则：只满足是 passives (通常是长文本) ---
-        else if (filterType === 'passives') {
+        else if ((filterType === 'passives') && !text.startsWith('-')) {
             const colonIndex = text.indexOf(':');
             const wideColonIndex = text.indexOf('：');
             let finalColonIndex = -1;
@@ -746,7 +746,7 @@ function applyKeywordHighlighting(text, lang, filterType) {
                 const partToHighlight = text.substring(0, finalColonIndex + 1);
                 const restOfText = text.substring(finalColonIndex + 1);
 
-                // 仅应用 yellow 高亮，并让文本继续后续处理
+                // 仅应用 orange 高亮，并让文本继续后续处理
                 textToProcess = `[##elementorange]${partToHighlight}[#]${restOfText}`;
             }
         }

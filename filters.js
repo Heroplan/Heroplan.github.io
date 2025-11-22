6276cdfc4e245d328a02ad9a35d04beeafba5a20
Heroplan <1622847724@qq.com>
2025-11-22 19:11:34 +0800

    Update filters.js

--- filters.js
+++ filters.js
@@ -816,19 +816,7 @@ function initializeNameAutocomplete() {
         langSelector.addEventListener('change', function () {
             const selectedLang = this.value;
             setCookie('search_lang', selectedLang, 365);
-            nameInput.value = ''; // 切换语言时清空输入
-            nameInput.focus();
-            if (selectedLang === 'current') {
-                loadData(state.currentLang);
-                // 延迟处理，确保已完成加载
-                setTimeout(() => {
-                    applyFiltersAndRender();
-                }, 500);
-            } else {
-                applyCustomLanguageNames(selectedLang);
-                applyFiltersAndRender();
-            }
-            updateClearButtonVisibility();
+            location.reload();
         });
     }
 

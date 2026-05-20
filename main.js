36e6ec12e4e33fec29d9c3bd16436eec1b7e5e43
Heroplan <1622847724@qq.com>
2026-05-20 17:21:55 +0800

    Update main.js

--- main.js
+++ main.js
@@ -52,7 +52,7 @@ function parseAndStoreDoTInfo(hero) {
 
         // 检查当前技能描述行是否满足某一组关键词共存的条件
         // ========== 多伤害专属规则 ==========
-        const mulDamages = (effectText.includes('共振') || effectText.includes('Resonance') || effectText.includes('Wild') || effectText.includes('荒野：')) && (effectText.includes('敌人') || effectText.includes('敵人') || effectText.includes('damage'));
+        const mulDamages = (effectText.includes('共振') || effectText.includes('Resonance') || effectText.includes('Wild') || effectText.includes('荒野：') || effectText.includes('所受伤害')) && (effectText.includes('敌人') || effectText.includes('敵人') || effectText.includes('damage'));
         if (mulDamages) {
             // 步骤1：强力清洗文本——剔除括号/星号/全角符号，替换全角空格为半角
             const cleanText = effectText
